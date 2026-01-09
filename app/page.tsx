9efbfe8f80165d6a082224e8c74e780072a2844f
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2026-01-09 18:37:48 -0500

    added the actual component to home lolll

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
+import AMLDashboard from "@/components/AMLInferencePanel";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold text-zinc-800 dark:text-zinc-100"></h1>
-      </main>
-    </div>
-  );
+  return <AMLDashboard />;
 }
